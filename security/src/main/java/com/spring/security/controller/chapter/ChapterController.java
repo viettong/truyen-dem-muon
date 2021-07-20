2f9764468027d935c8e3dba236e7bd16e8614c7c
Tong Duc Viet <hideonbush123456789@gmail.com>
2021-07-20 10:04:09 +0700

    update

--- security/src/main/java/com/spring/security/controller/chapter/ChapterController.java
+++ security/src/main/java/com/spring/security/controller/chapter/ChapterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ChapterController {
 
     @Autowired
